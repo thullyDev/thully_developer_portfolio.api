76cb5385596aa5899afa2091eef06b0a98b5d3c6
thullyDev <thully.developer@gmail.com>
2024-08-18 04:57:17 +0200

    [done] seems like i am done with the backend, so i am just going to test on the frontend

--- app/database/database.py
+++ app/database/database.py
@@ -7,22 +7,20 @@ from app.resources.misc import datenow
 client: MongoClient = MongoClient(DB_URL)
 db = client.test_database
 admin_collection = db.admins
-site_collection = db.site
+sites_collection = db.sites 
 projects_collection = db.projects
-# create admin
-# site data
-# project images
+
+
+def update_site_data(data: Dict[str, Any]) -> bool:
+	update_query = { "$set": { "data": data } }
+	response = projects_collection.update_one({ "site": "thully_developer_portfolio" }, update_query)
+
+	return response.acknowledged
 
 def create_admin(email: str, password: str) -> bool:
 	response = admin_collection.insert_one({ "email": email, "password": password, "session_token": "", "created_at": datenow() })
 	
 	return response.acknowledged
-
-def update_site_data(data: Dict[str, Any]):
-	update_query = { "$set": { "data": data } }
-	response = admin_collection.update_one({ "site": "thully_developer_portfolio" }, update_query)
-
-	return response
 
 def get_admin(email: str) -> Optional[Dict[str, Any]]:
 	admin = admin_collection.find_one({ "email": email })
@@ -49,4 +47,15 @@ def update_admin_token(email: str, session_token: str) -> bool:
 def upload_project(repo_slug: str, images: List[str]):
 	response = projects_collection.insert_one({ "images": images, "repo_slug": repo_slug,  "created_at": datenow() })
 
+	return response.acknowledged
+
+def update_project_images(repo_slug: str, images: List[str]) -> bool:
+	update_query = { "$set": { "images": images } }
+	response = projects_collection.update_one({ "repo_slug": repo_slug }, update_query)
+	
+	return response.acknowledged
+
+def delete_project(repo_slug: str) -> bool:
+	response = projects_collection.delete_one({ "repo_slug": repo_slug })
+	
 	return response.acknowledged
--- app/routers/router.py
+++ app/routers/router.py
@@ -1,4 +1,5 @@
 from typing import Any, List, Optional, Dict
+from typing_extensions import Tuple
 from fastapi import APIRouter
 from app.handlers import response_handler as response
 from app.database import database
@@ -6,8 +7,25 @@ from app.resources.config import SITE_KEY
 from app.resources.misc import generate_unique_token
 from app.handlers import storage
 import datetime
+import json
 
 router: APIRouter = APIRouter(prefix="/api")
+
+@router.get("/update_site_data/")
+def update_site_data(email: str, dataStr: str):
+     data = json.loads(dataStr)
+     images: List[str] = process_upload_profile_images(data["images"])
+     data["images"] = images
+     db_response = database.update_site_data(data)
+
+     if not db_response:
+          return response.crash_response(message="something went with updating the site data")
+
+     session_token = generate_unique_token()
+     database.update_admin_token(email=email, session_token=session_token)
+     
+     return response.successful_response(data={ "session_token": session_token })
+
 
 @router.get("/login/")
 def login(email: str, password: str):
@@ -51,9 +69,6 @@ def upload_project(email: str, repo_slug: str, images: str):
 
      image_urls = upload_images(string_images=images, repo_slug=repo_slug)
 
-     if not image_urls:
-          return response.crash_response(message="something went wrong with uploading the images")
-
      db_response = database.upload_project(repo_slug, images=image_urls)
 
      if not db_response:
@@ -73,8 +88,10 @@ def get_project(email: str, repo_slug: str):
 
      del project["_id"]
 
-     return response.successful_response(message="found", data=project)
-
+     session_token = generate_unique_token()
+     database.update_admin_token(email=email, session_token=session_token)
+     
+     return response.successful_response(data={ "project": project, "session_token": session_token })
 
 @router.get("/edit_project/{repo_slug}")
 def edit_project(email: str, repo_slug: str, images: str):
@@ -83,10 +100,8 @@ def edit_project(email: str, repo_slug: str, images: str):
      if not project:
           return response.not_found_response(message="project not found") 
 
-     project_images = project["images"]
-     new_project_images = process_project_images(images=images, project_images=project_images)
-
-     db_response = database.update_images(images=new_project_images)
+     new_project_images = upload_images(string_images=images, repo_slug=repo_slug)
+     db_response = database.update_project_images(images=new_project_images, repo_slug=repo_slug)
 
      if not db_response:
           return response.crash_response(message="something went wrong with trying to update images")
@@ -113,24 +128,46 @@ def delete_project(email: str, repo_slug: str):
      
      return response.successful_response(data={ "session_token": session_token })
      
-
-
-
-def upload_images(string_images: str, repo_slug: str) -> Optional[List[str]]:
-     spliter = "---***---" # never change this, it will conflict with the frontend
-     base64images = string_images.split(spliter) 
+def process_upload_profile_images(profile_images: Dict[str, str]) -> List[str]:
      image_urls = []
 
-     for index, value in enumerate(base64images):
-          name, base64image = process_image(image=value, name=f"{repo_slug}-image-{index}") 
-          image_url = storage.upload_base64_image(name=name, base64Str=base64image)
+     for key, image in profile_images.items():
+          if "https://" in image: 
+               # this condition is here for when its used in the edit, 
+               # it checks if the image from the image storage, 
+               # if so then it then it just appends it and moves on
+               image_urls.append(image)
+               continue
 
+          name, base64image = process_image(image=image, name=f"profile-image-{key}") 
+          image_url = storage.upload_base64_image(name=name, base64Str=base64image)
           image_urls.append(image_url)
 
      return image_urls
 
-def process_image(image: str, name: str):
+def upload_images(string_images: str, repo_slug: str) -> List[str]:
+     spliter = "---***---" # never change this, it will conflict with the frontend
+     base64images = string_images.split(spliter) 
+     image_urls = []
+
+     for index, image in enumerate(base64images):
+
+          if "https://" in image: 
+               # this condition is here for when its used in the edit, 
+               # it checks if the image from the image storage, 
+               # if so then it then it just appends it and moves on
+               image_urls.append(image)
+               continue
+
+          name, base64image = process_image(image=image, name=f"{repo_slug}-image-{index}") 
+          image_url = storage.upload_base64_image(name=name, base64Str=base64image)
+          image_urls.append(image_url)
+
+     return image_urls
+
+def process_image(image: str, name: str) -> Tuple[str, str]:
      current_time = datetime.datetime.now().strftime("%d-%m-%Y-%w-%d-%H-%M-%S-%f")
      name = f"{name}-{current_time}"
 
      return name, image.replace("data:image/jpeg;base64,", "").replace("data:image/png;base64,", "")
+
